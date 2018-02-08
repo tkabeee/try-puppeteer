6d498d881600a37d331848f00d3928e9ae020efc
tkabeee <tkabee@gmail.com>
2018-02-08 23:28:40 +0900

    package.json は appディレクトリの中にいれた方がよし

---
+++ app/script.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer')
+
+;(async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  })
+  const page = await browser.newPage()
+  await page.goto('https://www.google.co.jp')
+  await page.screenshot({
+    path: 'screenshot/googlecojp.png'
+  })
+
+  await browser.close()
+})()
